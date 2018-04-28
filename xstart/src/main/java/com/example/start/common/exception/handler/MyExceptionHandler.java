9faaed893eb06ca4cd600c17faea841538aae245
xynlanlan <xyn_369@163.com>
2018-04-28 18:07:04 +0800

    备注

--- xstart/src/main/java/com/example/start/common/exception/handler/MyExceptionHandler.java
+++ xstart/src/main/java/com/example/start/common/exception/handler/MyExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.example.start.common.exception.handler;
 
 import com.example.start.common.base.ResultMap;
-import com.example.start.common.constant.Constants;
 import com.example.start.common.exception.ExceptionCode;
-import com.example.start.common.exception.PageException;
 import com.example.start.common.exception.ServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
@@ -11,7 +9,6 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class MyExceptionHandler {
@@ -28,11 +25,5 @@ public class MyExceptionHandler {
     @ResponseBody
     public Map<String, Object> ExceptionHandler(ServiceException e) {
         return ResultMap.failure(((ServiceException) e).getCode(), e.getMessage(), e);
-    }
-    @ExceptionHandler(value = PageException.class)
-    public ModelAndView exception(PageException e) {
-        ModelAndView modelAndView = new ModelAndView("list.html");
-        modelAndView.addObject("error", e.getMessage());
-        return modelAndView;
     }
 }
